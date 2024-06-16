1f9ac28d48fb43f8f9bb680965237099f396715b
Pepe García <pepe@pepegar.com>
2024-06-16 15:46:36 +0200

    add cache to interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,21 +1,37 @@
+use tokio::sync::RwLock;
+use std::{collections::HashMap, sync::Arc};
 use anyhow::{Result, anyhow};
+use crate::commands::Command;
 
-use crate::commands;
-
+#[derive(Clone)]
 pub struct Interpreter {
-    
+    cache: Arc<RwLock<HashMap<String, String>>>
 }
 
 impl Interpreter {
-    pub fn respond(self: &Self, cmd: commands::Command) -> Result<commands::Command> {
+    pub async fn respond(self: &Self, cmd: Command) -> Result<Command> {
         match cmd {
-            commands::Command::PING => Ok(commands::Command::PONG),
-            commands::Command::ECHO(x) => Ok(commands::Command::ECHO(x)),
+            Command::PING => Ok(Command::PONG),
+            Command::ECHO(x) => Ok(Command::ECHO(x)),
+            Command::SET(key, value) => {
+                let mut map = self.cache.write().await;
+                map.insert(key, value);
+                Ok(Command::OK)
+            },
+            Command::GET(key) => {
+                let map = self.cache.read().await;
+                match map.get(key.as_str()) {
+                    Some(value) => Ok(Command::STR(value.to_owned())),
+                    None => Ok(Command::NIL),
+                }
+            },
             x => Err(anyhow!("unexpected command: {:?}", x))
         }
     }
 
     pub(crate) fn new() -> Interpreter {
-        Interpreter{}
+        Interpreter{
+            cache: Arc::new(RwLock::new(HashMap::new()))
+        }
     }
 }
